deac38068eddca1e2b509f4b3d91129ebd570cf6
Natnicha Faksang <omsin.natnicha@gmail.com>
2022-09-05 11:47:55 +0700

    add class student subject and register and create admin

--- registration/student/admin.py
+++ registration/student/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Student, Subject, Register
+
 # Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("username", "password", "name")
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ("subject_id", "subject_name", "semester", "year", "max_cap", "status")
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Subject, SubjectAdmin)
+admin.site.register(Register)
--- registration/student/views.py
+++ registration/student/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    pass
+    # login page
+    return HttpResponse('<h1>login</h1>')
+
+def reg(request):
+    # show all subject the user apply
+    return HttpResponse('<h1>Reg</h1>')
