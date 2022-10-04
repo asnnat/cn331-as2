de3b26adfe730e924077b8de0b9210a974f75350
Natnicha Faksang <omsin.natnicha@gmail.com>
2022-10-04 10:47:48 +0700

    add url test of user and regist

--- registration/regist/tests.py
+++ registration/regist/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client, SimpleTestCase
+from django.urls import reverse, resolve
+from .models import Student, Subject, Register
+from .views import index, subject, mysubject
 
-# Create your tests here.
+class TestUrl(SimpleTestCase):
+    def test_index_is_resolved(self):
+        url = reverse('regist:index')
+        self.assertEqual(resolve(url).func, index)
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        self.index_url = reverse('regist:index')
+
+    def test_index(self):
+        response = self.client.get(self.index_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'regist/index.html')
--- registration/users/tests.py
+++ registration/users/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from .views import index, login_view, logout_view
+
+class TestUrl(SimpleTestCase):
+    def test_index_is_resolved(self):
+        url = reverse('users:index')
+        self.assertEqual(resolve(url).func, index)
+
+    def test_login_is_resolved(self):
+        url = reverse('users:login')
+        self.assertEqual(resolve(url).func, login_view)
+
+    def test_logout_is_resolved(self):
+        url = reverse('users:logout')
+        self.assertEqual(resolve(url).func, logout_view)
+
+class TestViews(TestCase):
+    pass
